7f54b65d6b919a73b3b0d2609975ae7030fef017
Dhruw Vaviya <dhruwvaviya123@gmail.com>
2025-03-19 13:07:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,9 +2,23 @@ const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
+const table = document.getElementsByTagName("table")[0];
+
+let newRow = table.insertRow(-1);
+let newCell1 = newRow.insertCell(0);
+let newCell2 = newRow.insertCell(1);
+
+
+let totalPrice = 0;
 const getSum = () => {
 //Add your code here
-  
+  const prices = document.getElementsByClassName("price");
+	for(let i=0; i<prices.length; i++){
+		let price = parseInt(prices[i].innerText);
+		totalPrice += price;
+	};
+	newCell1.innerText = "Total Price";
+	newCell2.innerText = totalPrice;
 };
 
 getSumBtn.addEventListener("click", getSum);
